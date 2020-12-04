2aa2c826c70fc29064c053f1c019b8a71687277a
Simon Wanner <skyrising@pvpctutorials.de>
2020-12-04 11:39:18 +0100

    Day 4

--- src/main/kotlin/de/skyrising/aoc2020/aoc.kt
+++ src/main/kotlin/de/skyrising/aoc2020/aoc.kt
@@ -92,7 +92,7 @@ const val BENCHMARK = true
 fun main() {
     registerAll()
     for ((day, puzzles) in dailyPuzzles) {
-        if (day != 4) continue
+        // if (day != 4) continue
         println("Day $day:")
         for (puzzle in puzzles) {
             // if (!puzzle.getName().endsWith("v2")) continue
---
+++ src/main/kotlin/de/skyrising/aoc2020/day4.kt
@@ -0,0 +1,130 @@
+package de.skyrising.aoc2020
+
+class BenchmarkDay4 : BenchmarkDay(4)
+
+fun registerDay4() {
+    puzzleS(4, "Passport Processing v1") {
+        val fields = mutableSetOf<String>()
+        val required = setOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+        var valid = 0
+        for (line in it) {
+            if (line.isEmpty()) {
+                if (fields.containsAll(required)) valid++
+                fields.clear()
+            }
+            val parts = line.split(" ")
+            for (part in parts) {
+                if (part.isEmpty()) continue
+                fields.add(part.split(":")[0])
+            }
+        }
+        if (fields.containsAll(required)) valid++
+        valid
+    }
+    puzzleS(4, "Passport Processing v2") {
+        val fields = mutableSetOf<String>()
+        val required = setOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+        var valid = 0
+        for (line in it) {
+            if (line.isEmpty()) {
+                if (fields.size == 7) valid++
+                fields.clear()
+            }
+            val parts = line.split(" ")
+            for (part in parts) {
+                if (part.isEmpty()) continue
+                val field = part.split(":")[0]
+                if (field in required) fields.add(field)
+            }
+        }
+        if (fields.size == 7) valid++
+        valid
+    }
+    puzzleS(4, "Part Two v1") {
+        val fields = mutableSetOf<String>()
+        var valid = 0
+        val yearRegex = Regex("^\\d{4}$")
+        val hgtRegex = Regex("^(\\d+)(cm|in)$")
+        val hclRegex = Regex("^#[0-9a-f]{6}$")
+        val ecls = setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+        val pidRegex = Regex("^\\d{9}$")
+        for (line in it) {
+            if (line.isEmpty()) {
+                if (fields.size == 7) valid++
+                fields.clear()
+            }
+            val parts = line.split(" ")
+            for (part in parts) {
+                if (part.isEmpty()) continue
+                val (field, value) = part.split(":")
+                val fieldValid = when (field) {
+                    "byr" -> value.matches(yearRegex) && value.toInt() in 1920..2002
+                    "iyr" -> value.matches(yearRegex) && value.toInt() in 2010..2020
+                    "eyr" -> value.matches(yearRegex) && value.toInt() in 2020..2030
+                    "hgt" -> {
+                        val match = hgtRegex.matchEntire(value)
+                        if (match != null) {
+                            val h = match.groups[1]!!.value.toInt()
+                            when (match.groups[2]!!.value) {
+                                "cm" -> h in 150..193
+                                "in" -> h in 59..76
+                                else -> false
+                            }
+                        } else false
+                    }
+                    "hcl" -> value.matches(hclRegex)
+                    "ecl" -> value in ecls
+                    "pid" -> value.matches(pidRegex)
+                    else -> false
+                }
+                // println("$field ${if (fieldValid) "valid" else "invalid"}: $value")
+                if (fieldValid) fields.add(field)
+            }
+        }
+        if (fields.size == 7) valid++
+        valid
+    }
+    puzzleS(4, "Part Two v2") {
+        val fields = mutableSetOf<String>()
+        var valid = 0
+        val hclRegex = Regex("^#[0-9a-f]{6}$")
+        val ecls = setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+        val pidRegex = Regex("^\\d{9}$")
+        for (line in it) {
+            if (line.isEmpty()) {
+                if (fields.size == 7) valid++
+                fields.clear()
+            }
+            val parts = line.split(" ")
+            for (part in parts) {
+                if (part.isEmpty()) continue
+                val (field, value) = part.split(":")
+                val fieldValid = try {
+                    when (field) {
+                        "byr" -> value.toInt() in 1920..2002
+                        "iyr" -> value.toInt() in 2010..2020
+                        "eyr" -> value.toInt() in 2020..2030
+                        "hgt" -> {
+                            val h = value.substring(0, value.length - 2).toInt()
+                            when (value.substring(value.length - 2)) {
+                                "cm" -> h in 150..193
+                                "in" -> h in 59..76
+                                else -> false
+                            }
+                        }
+                        "hcl" -> value.matches(hclRegex)
+                        "ecl" -> value in ecls
+                        "pid" -> value.matches(pidRegex)
+                        else -> false
+                    }
+                } catch (_: NumberFormatException) {
+                    false
+                }
+                // println("$field ${if (fieldValid) "valid" else "invalid"}: $value")
+                if (fieldValid) fields.add(field)
+            }
+        }
+        if (fields.size == 7) valid++
+        valid
+    }
+}
