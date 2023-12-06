162e88973a469baeb5eb383ee5dce2cf94adc378
Simon Wanner <simon+git@skyrising.xyz>
2023-12-06 10:42:36 +0100

    Update 2019 Day 2

--- src/main/kotlin/de/skyrising/aoc2019/day2.kt
+++ src/main/kotlin/de/skyrising/aoc2019/day2.kt
@@ -1,7 +1,7 @@
 package de.skyrising.aoc2019
 
+import de.skyrising.aoc.PuzzleInput
 import de.skyrising.aoc.TestInput
-import de.skyrising.aoc.ints
 import de.skyrising.aoc.part1
 import de.skyrising.aoc.part2
 
@@ -13,19 +13,17 @@ fun registerDay2() {
     val test = TestInput("""
         1,9,10,3,2,3,11,0,99,30,40,50
     """)
+    fun run(input: PuzzleInput, noun: Int, verb: Int) = runIntcode(input) {
+        it[1] = noun
+        it[2] = verb
+    }.getInt(0)
     part1("Program Alarm") {
-        val code = string.ints()
-        code[1] = 12
-        code[2] = 2
-        runIntcode(code).getInt(0)
+        run(this, 12, 2)
     }
     part2 {
         for (noun in 0..99) {
             for (verb in 0..99) {
-                val code = string.ints()
-                code[1] = noun
-                code[2] = verb
-                if (runIntcode(code).getInt(0) == 19690720) {
+                if (run(this, noun, verb) == 19690720) {
                     return@part2 100 * noun + verb
                 }
             }
--- src/main/kotlin/de/skyrising/aoc2019/intcode.kt
+++ src/main/kotlin/de/skyrising/aoc2019/intcode.kt
@@ -8,22 +8,34 @@ const val OP_ADD = 1
 const val OP_MUL = 2
 const val OP_HALT = 99
 
+class Instruction(private val code: IntList, private val offset: Int) {
+    operator fun get(off: Int) = code.getInt(offset + off + 1)
+    val opcode get() = code.getInt(offset)
+    operator fun component1() = this[0]
+    operator fun component2() = this[1]
+    operator fun component3() = this[2]
+
+    val length get() = when (opcode) {
+        OP_ADD -> 4
+        OP_MUL -> 4
+        OP_HALT -> 1
+        else -> throw IllegalArgumentException("Invalid opcode $opcode")
+    }
+
+    val next get() = Instruction(code, offset + length)
+}
+
 fun runIntcode(code: IntList): IntList {
-    var pc = 0
+    var pc = Instruction(code, 0)
     while (true) {
-        when (code.getInt(pc)) {
-            OP_ADD -> {
-                code[code.getInt(pc + 3)] = code.getInt(code.getInt(pc + 1)) + code.getInt(code.getInt(pc + 2))
-                pc += 4
-            }
-            OP_MUL -> {
-                code[code.getInt(pc + 3)] = code.getInt(code.getInt(pc + 1)) * code.getInt(code.getInt(pc + 2))
-                pc += 4
-            }
+        when (pc.opcode) {
+            OP_ADD -> code[pc[2]] = code.getInt(pc[0]) + code.getInt(pc[1])
+            OP_MUL -> code[pc[2]] = code.getInt(pc[0]) * code.getInt(pc[1])
             OP_HALT -> return code
-            else -> throw IllegalArgumentException("Invalid opcode ${code.getInt(pc)}")
+            else -> throw IllegalArgumentException("Invalid opcode ${pc.opcode}")
         }
+        pc = pc.next
     }
 }
 
-fun runIntcode(input: PuzzleInput) = input.string.ints()
+fun runIntcode(input: PuzzleInput, modify: (IntList)->Unit = {}) = runIntcode(input.string.ints().also(modify))
