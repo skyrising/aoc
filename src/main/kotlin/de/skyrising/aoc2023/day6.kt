3ec66c2cce8ceb3697bfcb6914967a0a9ec76fbc
Simon Wanner <simon+git@skyrising.xyz>
2023-12-06 07:33:15 +0100

    Update Day 6

--- src/main/kotlin/de/skyrising/aoc2023/day6.kt
+++ src/main/kotlin/de/skyrising/aoc2023/day6.kt
@@ -4,6 +4,7 @@ import de.skyrising.aoc.TestInput
 import de.skyrising.aoc.ints
 import de.skyrising.aoc.part1
 import de.skyrising.aoc.part2
+import kotlin.math.sqrt
 
 @Suppress("unused")
 class BenchmarkDay6 : BenchmarkDayV1(6)
@@ -14,16 +15,18 @@ fun registerDay6() {
         Time:      7  15   30
         Distance:  9  40  200
     """)
-    part1("") {
+    fun wins(t: Long, d: Long): Long {
+        val t0 = ((t - sqrt(t*t-4.0*d)) / 2).toInt()
+        return t - t0 * 2 - 1
+    }
+    part1("Wait For It") {
         val (time, distance) = lines.map { it.ints() }
         time.indices.map {
-            val t = time.getInt(it)
-            val d = distance.getInt(it)
-            (0..t).count { t0 -> t0*(t-t0) > d }
-        }.reduce(Int::times)
+            wins(time.getInt(it).toLong(), distance.getInt(it).toLong())
+        }.reduce(Long::times)
     }
     part2 {
         val (t, d) = lines.map { it.filter(Char::isDigit).toLong() }
-        (0..t).count { t0 -> t0*(t-t0) > d }
+        wins(t, d)
     }
 }
