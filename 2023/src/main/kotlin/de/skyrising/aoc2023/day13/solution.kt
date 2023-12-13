e1b4431e0ddcfa9838972d085b1b1b19b717e058
Simon Wanner <simon+git@skyrising.xyz>
2023-12-13 16:33:35 +0100

    Update 2023 Day 13

--- 2023/src/main/kotlin/de/skyrising/aoc2023/day13/solution.kt
+++ 2023/src/main/kotlin/de/skyrising/aoc2023/day13/solution.kt
@@ -24,44 +24,28 @@ fun register() {
         ..##..###
         #....#..#
     """)
-    fun CharGrid.checkVertical(x: Int): Boolean {
-        for (offset in 0 until minOf(x, width - x)) {
-            for (y in 0 until height) if (this[x - offset - 1, y] != this[x + offset, y]) return false
-        }
-        return true
+    fun CharGrid.checkVertical(x: Int): Int = (0..<minOf(x, width - x)).sumOf {
+        (0..<height).count { y -> this[x - it - 1, y] != this[x + it, y] }
     }
-    fun CharGrid.checkHorizontal(y: Int): Boolean {
-        for (offset in 0 until minOf(y, height - y)) {
-            for (x in 0 until width) if (this[x, y - offset - 1] != this[x, y + offset]) return false
-        }
-        return true
+    fun CharGrid.checkHorizontal(y: Int): Int = (0..<minOf(y, height - y)).sumOf {
+        (0..<width).count { x -> this[x, y - it - 1] != this[x, y + it] }
     }
     part1("Point of Incidence") {
-        lines.splitOnEmpty().map(CharGrid.Companion::parse).sumOf {
-            val xm = (1 until it.width).firstOrNull(it::checkVertical) ?: 0
-            val ym = (1 until it.height).firstOrNull(it::checkHorizontal) ?: 0
-            xm + ym * 100
+        lines.splitOnEmpty().sumOf {
+            CharGrid.parse(it).run {
+                val xm = (1 until width).firstOrNull { x -> checkVertical(x) == 0 } ?: 0
+                val ym = (1 until height).firstOrNull { y -> checkHorizontal(y) == 0 } ?: 0
+                xm + ym * 100
+            }
         }
     }
     part2 {
-        lines.splitOnEmpty().map(CharGrid.Companion::parse).sumOf {
-            val xm0 = (1 until it.width).firstOrNull(it::checkVertical) ?: 0
-            val ym0 = (1 until it.height).firstOrNull(it::checkHorizontal) ?: 0
-            for (sx in 0 until it.width) {
-                for (sy in 0 until it.height) {
-                    val prev = it[sx, sy]
-                    it[sx, sy] = if(prev == '.') '#' else '.'
-                    val xm = (1 until it.width).firstOrNull { x ->
-                        x != xm0 && it.checkVertical(x)
-                    } ?: 0
-                    val ym = (1 until it.height).firstOrNull { y ->
-                        y != ym0 && it.checkHorizontal(y)
-                    } ?: 0
-                    if (xm != 0 || ym != 0) return@sumOf xm + ym * 100
-                    it[sx, sy] = prev
-                }
+        lines.splitOnEmpty().sumOf {
+            CharGrid.parse(it).run {
+                val xm = (1 until width).firstOrNull { x -> checkVertical(x) == 1 } ?: 0
+                val ym = (1 until height).firstOrNull { y -> checkHorizontal(y) == 1 } ?: 0
+                xm + ym * 100
             }
-            0
         }
     }
 }
