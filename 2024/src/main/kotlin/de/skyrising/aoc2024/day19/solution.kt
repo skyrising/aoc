c10daae2bd1c7d2740787a05ec1afc6a7c906ab1
Simon Wanner <simon+git@skyrising.xyz>
2024-12-19 11:35:56 +0100

    2024 Day 19

---
+++ 2024/src/main/kotlin/de/skyrising/aoc2024/day19/solution.kt
@@ -0,0 +1,47 @@
+package de.skyrising.aoc2024.day19
+
+import de.skyrising.aoc.PuzzleInput
+import de.skyrising.aoc.PuzzleName
+import de.skyrising.aoc.TestInput
+import de.skyrising.aoc.splitOnEmpty
+import it.unimi.dsi.fastutil.objects.Object2LongMap
+import it.unimi.dsi.fastutil.objects.Object2LongOpenHashMap
+
+val test = TestInput("""
+r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb
+""")
+
+fun make(target: String, patterns: List<String>, memo: Object2LongMap<String> = Object2LongOpenHashMap()): Long {
+    if (target.isEmpty()) return 1
+    if (memo.containsKey(target)) return memo.getLong(target)
+    var count = 0L
+    for (pattern in patterns) {
+        if (target.startsWith(pattern)) {
+            count += make(target.substring(pattern.length), patterns, memo)
+        }
+    }
+    memo.put(target, count)
+    return count
+}
+
+@PuzzleName("Linen Layout")
+fun PuzzleInput.part1(): Any {
+    val (patternsS, designs) = lines.splitOnEmpty(2)
+    val patterns = patternsS.single().split(", ")
+    return designs.count { make(it, patterns) > 0 }
+}
+
+fun PuzzleInput.part2(): Any {
+    val (patternsS, designs) = lines.splitOnEmpty(2)
+    val patterns = patternsS.single().split(", ")
+    return designs.sumOf { make(it, patterns) }
+}
