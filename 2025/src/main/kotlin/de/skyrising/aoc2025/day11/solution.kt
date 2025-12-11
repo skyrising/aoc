54cb80e67f0f0b853c00de3806d7ff4babdf6db3
Simon Wanner <simon+git@skyrising.xyz>
2025-12-11 22:42:11 +0100

    Optimize 2025 Day 11

--- 2025/src/main/kotlin/de/skyrising/aoc2025/day11/solution.kt
+++ 2025/src/main/kotlin/de/skyrising/aoc2025/day11/solution.kt
@@ -1,12 +1,16 @@
 @file:PuzzleName("Reactor")
+@file:Suppress("NOTHING_TO_INLINE")
 
 package de.skyrising.aoc2025.day11
 
 import de.skyrising.aoc.PuzzleInput
 import de.skyrising.aoc.PuzzleName
 import de.skyrising.aoc.TestInput
-import de.skyrising.aoc.forEachInt
-import it.unimi.dsi.fastutil.ints.*
+import de.skyrising.aoc.toIntExact
+import it.unimi.dsi.fastutil.shorts.Short2ShortFunction
+import it.unimi.dsi.fastutil.shorts.Short2ShortOpenHashMap
+import it.unimi.dsi.fastutil.shorts.ShortArrayList
+import it.unimi.dsi.fastutil.shorts.ShortList
 
 val test = TestInput("""
 aaa: you hhh
@@ -37,55 +41,75 @@ ggg: out
 hhh: out
 """)
 
-fun Int2ObjectMap<IntSet>.countPaths(from: Int, to: Int, cache: Int2LongMap = Int2LongOpenHashMap(size)): Long {
+fun Prepared.countPaths(from: Short, to: Short, cache: IntArray = IntArray(size) { -1 }): Int {
     if (from == to) return 1
-    val known = cache.getOrDefault(to, -1)
-    if (known != -1L) return known
+    val known = cache[to.toInt()]
+    if (known != -1) return known
     var count = 0L
-    val incoming = this[to]
-    if (incoming != null) {
-        incoming.forEachInt {
-            count += countPaths(from, it, cache)
-        }
+    val incoming = incoming[to.toInt()]
+    for (i in incoming.indices) {
+        count += countPaths(from, incoming.getShort(i), cache)
     }
-    cache.put(to, count)
-    return count
+    cache[to.toInt()] = count.toIntExact()
+    return count.toInt()
 }
 
-fun PuzzleInput.prepare(): Int2ObjectMap<IntSet> {
+class Prepared(
+    val incoming: Array<ShortList>,
+    val size: Int,
+    val you: Short,
+    val out: Short,
+    val svr: Short,
+    val dac: Short,
+    val fft: Short,
+)
+
+inline fun compress(value: Int): Short {
+    val a = value.shr(16).and(31)
+    val b = value.shr(8).and(31)
+    val c = value.and(31)
+    return (a.shl(10) or b.shl(5) or c).toShort()
+}
+
+inline fun Short2ShortFunction.id(value: Int): Short {
+    val key = compress(value)
+    var i = getOrDefault(key, -1)
+    if (i < 0) {
+        i = size().toShort()
+        put(key, i)
+    }
+    return i
+}
+
+fun PuzzleInput.prepare(): Prepared {
     val lines = byteLines
-    val incoming = Int2ObjectOpenHashMap<IntSet>(lines.size)
+    val ids = Short2ShortOpenHashMap(lines.size + 1)
+    val incoming = Array<ShortList>(lines.size + 1) { ShortArrayList(23) }
     for (line in lines) {
-        val from = line.getInt(0) shr 8
-        val limit = line.limit()
-        var i = 4
-        while (i < limit) {
-            val to = line.getInt(i) and 0xffffff
-            var set = incoming[to]
-            if (set == null) {
-                set = IntArraySet()
-                incoming.put(to, set)
-            }
-            set.add(from)
-            i += 4
+        val from = ids.id(line.getInt(0) shr 8)
+        for (i in 1 ..< line.limit() / 4) {
+            val to = ids.id(line.getInt(i * 4) and 0xffffff)
+            incoming[to.toInt()].add(from)
         }
     }
-    return incoming
+    return Prepared(
+        incoming,
+        ids.size,
+        ids.id('y'.code.shl(16) or 'o'.code.shl(8) or 'u'.code),
+        ids.id('o'.code.shl(16) or 'u'.code.shl(8) or 't'.code),
+        ids.id('s'.code.shl(16) or 'v'.code.shl(8) or 'r'.code),
+        ids.id('d'.code.shl(16) or 'a'.code.shl(8) or 'c'.code),
+        ids.id('f'.code.shl(16) or 'f'.code.shl(8) or 't'.code),
+    )
 }
 
-const val YOU = 'y'.code.shl(16) or 'o'.code.shl(8) or 'u'.code
-const val OUT = 'o'.code.shl(16) or 'u'.code.shl(8) or 't'.code
-const val SVR = 's'.code.shl(16) or 'v'.code.shl(8) or 'r'.code
-const val DAC = 'd'.code.shl(16) or 'a'.code.shl(8) or 'c'.code
-const val FFT = 'f'.code.shl(16) or 'f'.code.shl(8) or 't'.code
+fun Prepared.part1() = countPaths(you, out)
 
-fun Int2ObjectMap<IntSet>.part1() = countPaths(YOU, OUT)
-
-fun Int2ObjectMap<IntSet>.part2(): Long {
-    val dacFft = countPaths(DAC, FFT)
-    return if (dacFft != 0L) {
-        countPaths(SVR, DAC) * dacFft * countPaths(FFT, OUT)
+fun Prepared.part2(): Long {
+    val fftDac = countPaths(fft, dac)
+    return if (fftDac != 0) {
+        countPaths(svr, fft).toLong() * fftDac * countPaths(dac, out)
     } else {
-        countPaths(SVR, FFT) * countPaths(FFT, DAC) * countPaths(DAC, OUT)
+        countPaths(svr, dac).toLong() * countPaths(dac, fft) * countPaths(fft, out)
     }
 }
